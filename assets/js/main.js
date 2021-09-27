5ee100a056feaef41f6adf8ec3a01f083aa3c80e
Ken Karlson <kenkarlson@mail.ru>
2021-09-27 23:39:32 +0300

    fix all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,28 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
-  effect: "fade",
+  effect: "coverflow",
   keyboard: {
     enable: true,
     onlyInViewport: true,
   },
-  mousewheel: {
-    sensitivity: 1,
-    eventTarget: ".swiper",
-  }
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: {
+    enable: true,
+    onlyInViewport: true,
+  },
 });
