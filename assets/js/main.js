bb9246c64b86f67b8f0f8ffbc4e3effd6adc782b
Ken Karlson <kenkarlson@mail.ru>
2021-10-01 19:39:52 +0300

    create: mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,8 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: true,
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function () {
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
